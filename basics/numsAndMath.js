4d2c68141572a3ae0afab0a68cb6e429e94a3d24
Ashish Kumar <ak1216377@gmail.com>
2026-02-01 09:49:17 +0000

    container config done

---
+++ basics/numsAndMath.js
@@ -0,0 +1,15 @@
+const score = 100
+const balance = new Number(400)
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed())
+
+const otherNumber = 123.678
+console.log(otherNumber.toPrecision(4))
+
+const hundred = 100000
+console.log(hundred.toLocaleString())
+
+
+

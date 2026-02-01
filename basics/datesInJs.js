a6390af19c10bf6a176cc051583731f9786fc8c4
Ashish Kumar <ak1216377@gmail.com>
2026-02-01 14:44:11 +0000

    container config done

---
+++ basics/datesInJs.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toISOString())
+
+//let myCreatedDate = new Date(2023, 0 , 23)//single digit me zero ko array ki tarah liya jata hai ise january se pretend kiya jata hai
+//console.log(myCreatedDate.toLocaleString())
+//let myCreatedDate = new Date(2023, 0 , 23,5,3)
+//console.log(myCreatedDate.toLocaleString())
+
+let myCreatedDate = new Date("2026-02-01")//string me month ko 1 se start kiya jata hai
+console.log(myCreatedDate.toLocaleDateString())
+
+
+
